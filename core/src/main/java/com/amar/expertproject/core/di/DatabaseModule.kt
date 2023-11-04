6dc9dfe99ebc783cb81014ee26773641a929e221
amarsh30 <rzhamr06@gmail.com>
2023-11-04 09:49:31 +0700

    add encryption pada database

--- core/src/main/java/com/amar/expertproject/core/di/DatabaseModule.kt
+++ core/src/main/java/com/amar/expertproject/core/di/DatabaseModule.kt
@@ -9,6 +9,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,11 +19,17 @@ class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context): RestaurantDatabase =
-        Room.databaseBuilder(
+    fun provideDatabase(@ApplicationContext context: Context): RestaurantDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("restaurant".toCharArray())
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(
             context,
             RestaurantDatabase::class.java, "Restaurant.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
+    }
+
 
     @Provides
     fun provideRestaurantDao(database: RestaurantDatabase): RestaurantDao = database.restaurantDao()
